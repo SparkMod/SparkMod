e966a19071f59d972c4b0ba512dbcb356d11a28a
bawNg <bawng@intoxicated.co.za>
2013-05-05 02:59:34 +0200

    corrected inconsistent indentation

--- source/lua/PreLoadMod.lua
+++ source/lua/PreLoadMod.lua
@@ -1,7 +1,7 @@
 -- SparkMod preloader
 
 if Server then
-	Script.Load("lua/SparkMod/server.lua")
+    Script.Load("lua/SparkMod/server.lua")
 elseif Client then
     Script.Load("lua/SparkMod/client.lua")
 end
