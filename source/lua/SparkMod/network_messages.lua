f7d634c5b18046b2cbc90a55fafbd4f95299209a
bawNg <bawng@intoxicated.co.za>
2013-05-09 17:24:29 +0200

    client shouldn't detect workshop support

--- source/lua/SparkMod/network_messages.lua
+++ source/lua/SparkMod/network_messages.lua
@@ -126,7 +126,7 @@ function SparkMod.NetworkMessage(message_name, func)
     func()
 end
 
-if not SparkMod.is_using_workshop then return end
+if Server and not SparkMod.is_using_workshop then return end
 
 -- Core network messages
 kMaxConfigValueLength = 256
